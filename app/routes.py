3e91fab3735013d2137780943449808d3feb68dd
ADOwen <a.einhorn886@gmail.com>
2021-12-08 16:02:10 -0500

    fix 2

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,12 @@
 from app import app
 from flask import render_template
-from livereload import server
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', my_title = 'Home', name = 'Allan', my_list= ['Allan','Mike','Chris',"Jay", "Booms"])
+    return render_template('index.html', my_title = 'Home', my_list= ['Games','Electronics','Accesories', 'And More!'])
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
 
